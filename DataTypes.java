e10fd545ff2fd549886b3fafc6452eb062a7c352
iamnicky <2020ugee067@nitjsr.ac.in>
2023-12-13 18:15:30 +0530

    dataTypes added

--- DataTypes.java
+++ DataTypes.java
@@ -1,5 +1,16 @@
 public class DataTypes {
-    public static void main(String[] args) {
-
-    }
+    /* Data Types are of two types
+          1) Primitive DataTypes : pre-defined reserved Keywords
+          2) Non-Primitive DataTypes : user-defined keywords
+    */
+    //Some primitive Data Type
+    int n = 3;
+    long l = 200;
+    float fn = 2.20f;
+    double dn = 20.33d;
+    boolean b = true;
+    char c = 'a';
+    // and many more are there
+    //examples of non-primitives are
+    //String and array
 }
